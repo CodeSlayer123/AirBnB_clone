403b54257aa872ce126c79a227c7235486652104
Joseph W <3599@holbertonschool.com>
2022-02-02 13:02:37 -0600

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,20 @@ from datetime import datetime
 class BaseModel():
     """defines all common attributes/methods for other classes"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at":
+                        self.created_at = datetime.fromisoformat(value)
+                    elif key == "updated_at":
+                        self.updated_at = datetime.fromisoformat(value)
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format("BaseModel", self.id, self.__dict__)
